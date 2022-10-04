669eb56949e7dd979a4ea0156cdf34b0ac13d720
ibnabii <bla@bla.pl>
2022-10-04 12:48:51 +0200

    Stage 2/6

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,12 +1,28 @@
 // Use "input()" to input a line from the user
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
-const input = require('sync-input')
+const input = require('sync-input');
 
-console.log(`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`);
+function howMuchCoffee () {
+    console.log("Write how many cups of coffee you will need:");
+    return input();
+}
+
+function calculateIngredients (cups) {
+    let ingredients = {
+        water: 200 * cups,
+        milk: 50 * cups,
+        coffeeBeans: 15 * cups
+    };
+    return ingredients;
+}
+
+function printIngredients (ingredients) {
+    console.log(`${ingredients.water} ml of water`);
+    console.log(`${ingredients.milk} ml of milk`);
+    console.log(`${ingredients.coffeeBeans} ml of coffee beans`);
+}
+
+let cups = howMuchCoffee();
+console.log(`For ${cups} cups of coffee you will need:`);
+printIngredients(calculateIngredients(cups));
